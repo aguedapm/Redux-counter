0a8fc0ec104aee7af7b4871d8255de247d86f4e4
Agueda Perez <aguedapm@yahoo.com>
2019-07-01 17:07:24 +0200

    [Code] Added state.json from Redux Dev Tools

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,24 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import logger from 'redux-logger';
 import reducers from '../reducers';
 
 const middleware = [
     logger
 ]
-const store = createStore(reducers, applyMiddleware(...middleware));
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
+
+
+const store = createStore(reducers, enhancer);
 
 export default store;
